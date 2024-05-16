aba80217b1539e27eaea7cffe9ebb6917e301395
NicolasLemaAlves <nicolas.lema@etml-es.ch>
2024-05-16 17:18:34 +0200

    fix : Modify Reservation

---
+++ projet_csharp_travel_plan/projet_csharp_travel_plan_frontend/DTO/ReservationCreateViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace projet_csharp_travel_plan_frontend.DTO
+{
+    public class ReservationCreateViewModel
+    {
+        public string SelectedCountry { get; set; }
+        public string SelectedOption { get; set; }
+    }
+}
