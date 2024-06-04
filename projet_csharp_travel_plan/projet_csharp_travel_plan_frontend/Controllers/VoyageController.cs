74bc2a88a614a75ae28a99fdf28c4e5c61cd515b
julcobas <juliecobas@outlook.com>
2024-06-04 15:42:56 +0200

    fix : Test Redirection vers route Réservation / Create

--- projet_csharp_travel_plan/projet_csharp_travel_plan_frontend/Controllers/VoyageController.cs
+++ projet_csharp_travel_plan/projet_csharp_travel_plan_frontend/Controllers/VoyageController.cs
@@ -73,7 +73,7 @@ namespace projet_csharp_travel_plan_frontend.Controllers
             return View("Error", errorModel);
         }
 
-        // POST: Voyage/Create
+        /// POST: Voyage/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(VoyageDTO voyage)
@@ -84,9 +84,9 @@ namespace projet_csharp_travel_plan_frontend.Controllers
                 var content = new StringContent(json, Encoding.UTF8, "application/json");
 
                 var response = await _client.PostAsync(API_URL, content);
-               if (response.IsSuccessStatusCode)
+                if (response.IsSuccessStatusCode)
                 {
-                    return RedirectToAction("Create", "Reservations", new { voyage.IdPays, voyage.IdVoyage });
+                    return RedirectToAction("Index", "Reservation", new { id = voyage.IdPays });
                 }
 
                 // Handle error
