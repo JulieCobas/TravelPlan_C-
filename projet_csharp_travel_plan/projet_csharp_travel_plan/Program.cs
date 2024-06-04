825a1705e9c44e48872c05f62197e00ae4e35a22
NicolasLemaAlves <nicolas.lema@etml-es.ch>
2024-06-04 10:53:21 +0200

    problem in the program

--- projet_csharp_travel_plan/projet_csharp_travel_plan/Program.cs
+++ projet_csharp_travel_plan/projet_csharp_travel_plan/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers(x => x.Filters.Add<ApiKeyAuthFilter>());
 builder.Services.AddDbContext<TravelPlanNewDbContext>(options =>
-     options.UseSqlServer(builder.Configuration.GetConnectionString("dbcon")));
+     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
